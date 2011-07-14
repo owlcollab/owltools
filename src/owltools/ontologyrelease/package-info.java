b779c854e550ece6dfb0dea5c4e642f4304055d8
Chris Mungall <cjm@berkeleybop.org>
2011-07-14 23:01:14 +0000

    git-svn-id: http://owltools.googlecode.com/svn/trunk@48 18f1da76-1bb4-b526-5913-e828fe20442d

--- src/owltools/ontologyrelease/package-info.java
+++ src/owltools/ontologyrelease/package-info.java
@@ -3,7 +3,22 @@
 
    <h3>Building the release manager</h3>
 
-   Eventually this will have its own installer. Developers do this to build:
+   Eventually this will have its own installer.
+
+   For now, make sure you have the most up to date version:
+
+   <pre>
+   cd OWLTools
+   svn update
+   </pre>
+
+   The oboformat.jar should be up to date - but to be sure:
+
+   <pre>
+   cp ../oboformat/lib/oboformat.jar runtime/
+   </pre>
+
+   Developers do this to build (for the OWLTools dir):
 
    <pre>
    ant ontology-release-jar
