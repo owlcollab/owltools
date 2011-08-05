8c71ec2cb9223b96408a6dad3943c16f29211f6c
shahid.manzoor@gmail.com <shahid.manzoor@gmail.com>
2011-08-05 13:44:51 +0000

    gaf parser and objects builder is added
    git-svn-id: http://owltools.googlecode.com/svn/trunk@158 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ src/owltools/gaf/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains gaf file parser {@link owltools.gaf.GAFParser}, and also contains {@link owltools.gaf.GafObjectsBuilder} to
+ * build Document Object Model from a gaf file. See {@link owltools.gaf.test.GAFParserTest} and {@link owltools.gaf.test.GafObjectsBuilderTest}
+ * classes to see examples how to use GafParser and GafObjectsBuilder classes. 
+ */
+package owltools.gaf;
+
