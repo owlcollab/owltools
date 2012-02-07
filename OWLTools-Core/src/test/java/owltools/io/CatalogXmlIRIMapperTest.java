af550f6b5ad1d0a3959146c140310ccf432a58a3
Heiko Dietze <hdietze@lbl.gov>
2012-02-07 22:31:36 +0000

    extend test case for IRI mapper
    git-svn-id: http://owltools.googlecode.com/svn/trunk@571 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Core/src/test/java/owltools/io/CatalogXmlIRIMapperTest.java
+++ OWLTools-Core/src/test/java/owltools/io/CatalogXmlIRIMapperTest.java
@@ -9,12 +9,16 @@ import java.util.Map;
 
 import org.junit.Test;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyLoaderListener;
 
 import owltools.OWLToolsTestBasics;
 import owltools.io.CatalogXmlIRIMapper;
 
 public class CatalogXmlIRIMapperTest extends OWLToolsTestBasics {
 
+	private static boolean verbose = false;
+	
 	@Test
 	public void testParseCatalogXML() throws Exception {
 		File resource = getResource("catalog-v001.xml");
@@ -30,5 +34,43 @@ public class CatalogXmlIRIMapperTest extends OWLToolsTestBasics {
 		IRI iri = m.getDocumentIRI(IRI.create("http://purl.obolibrary.org/obo/go.owl"));
 		assertNotNull(iri);
 	}
+	
+	@Test
+	public void testParseCatalogXML3() throws Exception {
+		CatalogXmlIRIMapper m = new CatalogXmlIRIMapper("src/test/resources/catalog-v001.xml");
+		ParserWrapper p = new ParserWrapper();
+		p.addIRIMapper(m);
+		if (verbose) {
+			p.manager.addOntologyLoaderListener(new PrintingOntologLoaderListener());
+		}
+		OWLOntology owlOntology = p.parse(getResourceIRIString("wine.owl"));
+		assertNotNull(owlOntology);
+	}
+	
+	@Test
+	public void testParseCatalogXML4() throws Exception {
+		CatalogXmlIRIMapper m = new CatalogXmlIRIMapper("src/test/resources/catalog-v001.xml");
+		ParserWrapper p = new ParserWrapper();
+		p.addIRIMapper(m);
+		if (verbose) {
+			p.manager.addOntologyLoaderListener(new PrintingOntologLoaderListener());
+		}
+		OWLOntology owlOntology = p.parse(getResourceIRIString("mutual-import-1.owl"));
+		assertNotNull(owlOntology);
+	}
 
+	public static final class PrintingOntologLoaderListener implements
+			OWLOntologyLoaderListener {
+		@Override
+		public void startedLoadingOntology(LoadingStartedEvent event) {
+			System.out.println("Loading: "+event.getDocumentIRI());
+			
+		}
+	
+		@Override
+		public void finishedLoadingOntology(LoadingFinishedEvent event) {
+			System.out.println("Finished: "+event.getDocumentIRI());
+		}
+	}
+	
 }
