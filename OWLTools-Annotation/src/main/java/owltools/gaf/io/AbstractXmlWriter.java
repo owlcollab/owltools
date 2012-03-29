af5222b327165eea53d2aacbd1a9319d040d70b2
sjcarbon@lbl.gov <sjcarbon@lbl.gov>
2012-03-29 21:29:37 +0000

    comments wrap
    git-svn-id: http://owltools.googlecode.com/svn/trunk@635 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Annotation/src/main/java/owltools/gaf/io/AbstractXmlWriter.java
+++ OWLTools-Annotation/src/main/java/owltools/gaf/io/AbstractXmlWriter.java
@@ -74,7 +74,15 @@ public abstract class AbstractXmlWriter {
 
 				depth++;
 			}
+			
+			// Match comments to the current depth.
+			if ("writeComment".equals(m)) {
+				// indent for current depth
+				target.writeCharacters(LINEFEED_CHAR);
+				target.writeCharacters(repeat(depth, indentString));
+			}
 
+			
 			if ("writeEndElement".equals(m)) {
 				depth--;
 
