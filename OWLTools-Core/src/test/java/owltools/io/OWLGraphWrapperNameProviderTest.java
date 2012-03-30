fdb168fd9990d52a78ecc196dc07b515b2a344fc
Heiko Dietze <hdietze@lbl.gov>
2012-03-30 23:38:48 +0000

    fix test case
    git-svn-id: http://owltools.googlecode.com/svn/trunk@647 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Core/src/test/java/owltools/io/OWLGraphWrapperNameProviderTest.java
+++ OWLTools-Core/src/test/java/owltools/io/OWLGraphWrapperNameProviderTest.java
@@ -1,39 +1,23 @@
 package owltools.io;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.assertTrue;
-import static org.junit.Assert.*;
+import static junit.framework.Assert.*;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.List;
 
 import org.coode.owlapi.obo.parser.OBOOntologyFormat;
 import org.junit.Test;
 import org.obolibrary.obo2owl.Obo2Owl;
 import org.obolibrary.oboformat.model.OBODoc;
 import org.obolibrary.oboformat.parser.OBOFormatParser;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLObject;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import owltools.OWLToolsTestBasics;
-import owltools.graph.OWLGraphEdge;
 import owltools.graph.OWLGraphWrapper;
-import owltools.graph.OWLGraphWrapper.ISynonym;
-import owltools.io.OWLGsonRenderer;
 
 public class OWLGraphWrapperNameProviderTest extends OWLToolsTestBasics {
 
@@ -62,7 +46,7 @@ public class OWLGraphWrapperNameProviderTest extends OWLToolsTestBasics {
 			if (line.startsWith("id: GO:0043066 ! negative regulation of apoptosis")) {
 				n++;
 			}
-			if (line.startsWith("GO:0043069 ! negative regulation of programmed cell death")) {
+			if (line.startsWith("id: GO:0043069 ! negative regulation of programmed cell death")) {
 				n++;
 			}
 		}
@@ -71,9 +55,6 @@ public class OWLGraphWrapperNameProviderTest extends OWLToolsTestBasics {
 
 	}
 
-
-
-	
 	private OWLGraphWrapper getOBO2OWLOntologyWrapper(String file) throws OWLOntologyCreationException, FileNotFoundException, IOException{
 		OBOFormatParser p = new OBOFormatParser();
 		OBODoc obodoc = p.parse(new BufferedReader(new FileReader(getResource(file))));
