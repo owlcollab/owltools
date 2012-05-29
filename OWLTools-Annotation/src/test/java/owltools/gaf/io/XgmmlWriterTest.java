7a7dfea2bb2ee17d6ac049b2c3a092ab739e1789
Heiko Dietze <hdietze@lbl.gov>
2012-05-29 18:18:33 +0000

    make sysout optional
    git-svn-id: http://owltools.googlecode.com/svn/trunk@734 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Annotation/src/test/java/owltools/gaf/io/XgmmlWriterTest.java
+++ OWLTools-Annotation/src/test/java/owltools/gaf/io/XgmmlWriterTest.java
@@ -1,17 +1,10 @@
 package owltools.gaf.io;
 
-import static org.junit.Assert.*;
-import static owltools.gaf.io.XgmmlWriter.*;
-
+import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
 
-import javax.xml.stream.XMLStreamWriter;
-
-import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.semanticweb.owlapi.model.OWLClass;
 
 import owltools.OWLToolsTestBasics;
 import owltools.gaf.GafDocument;
@@ -23,6 +16,11 @@ import owltools.io.ParserWrapper;
  * Tests for {@link XgmmlWriter}.
  */
 public class XgmmlWriterTest extends OWLToolsTestBasics {
+	
+	/*
+	 * If required, set this flag to true, to print the generated output.
+	 */
+	private static final boolean printOutput = false;
 
 	private static final ParserWrapper pw = new ParserWrapper();
 	private static OWLGraphWrapper graph = null;
@@ -39,7 +37,16 @@ public class XgmmlWriterTest extends OWLToolsTestBasics {
 		GafDocument gaf = builder.buildDocument(getResource("test_gene_association_mgi.gaf"));
 		
 		XgmmlWriter w = new XgmmlWriter();
-		w.write(System.out, graph, Arrays.asList(gaf));
+		
+		// use byte array as temp buffer
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		w.write(out, graph, Arrays.asList(gaf));
+		out.close();
+		
+		if (printOutput) {
+			// convert byte array into a String and print
+			System.out.println(new String(out.toByteArray()));
+		}
 	}
 
 }
