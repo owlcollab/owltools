4d52c940c2534f52d2c21bbf6715dfe5c2f2957c
Chris Mungall <cjm@berkeleybop.org>
2012-06-01 18:00:55 +0000

    caches stdout
    git-svn-id: http://owltools.googlecode.com/svn/trunk@746 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Runner/contrib/make-obo-library-release-makefile.pl
+++ OWLTools-Runner/contrib/make-obo-library-release-makefile.pl
@@ -79,7 +79,7 @@ foreach my $ns (keys %d)  {
     push(@rules, "$srcf: stamp\n\twget --no-check-certificate '$d{$ns}' -O \$@.tmp && ( cmp \$@.tmp \$@ && echo identical || cp \$@.tmp \$@)");
 
     # then build
-    push(@rules, "$ont/$ont.owl: $srcf\n\tontology-release-runner --skip-release-folder --skip-format owx --allow-overwrite --outdir $ont --no-reasoner --asserted --simple \$<");
+    push(@rules, "$ont/$ont.owl: $srcf\n\tontology-release-runner --skip-release-folder --skip-format owx --allow-overwrite --outdir $ont --no-reasoner --asserted --simple \$< > \$@.tmp && mv \$@.tmp \$@.log");
     push(@rules, "$ont/$ont.obo: $ont/$ont.owl");
 
     # then release
