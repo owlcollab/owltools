a463eb44034fe0b82fe2bd01433ae46d74daf7d5
Chris Mungall <cjm@berkeleybop.org>
2012-10-02 21:06:32 +0000

    git-svn-id: http://owltools.googlecode.com/svn/trunk@943 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Runner/src/test/java/owltools/cli/CommandRunnerMooncatTest.java
+++ OWLTools-Runner/src/test/java/owltools/cli/CommandRunnerMooncatTest.java
@@ -15,10 +15,18 @@ import owltools.OWLToolsTestBasics;
  * no attempt made to check results
  * 
  */
-public class CommandRunnerTest extends AbstractCommandRunnerTest {
+public class CommandRunnerMooncatTest extends AbstractCommandRunnerTest {
 	
 	@Test
 	public void testRunner() throws Exception {
+		init();
+		load("ceph.obo");
+		run("-a tentacle");
+		
+		run("--make-subset-by-properties part_of // -o -f obo /tmp/foo.obo");
+		
+		//run("-o -f obo /tmp/foo.obo");
+		
 	}
 	
 	
