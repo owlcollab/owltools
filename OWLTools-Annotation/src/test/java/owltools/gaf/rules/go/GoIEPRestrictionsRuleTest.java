85efc984c7f4ed775870e124852402d286354787
Heiko Dietze <hdietze@lbl.gov>
2012-10-12 21:36:01 +0000

    fix resource issue in tests
    git-svn-id: http://owltools.googlecode.com/svn/trunk@964 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Annotation/src/test/java/owltools/gaf/rules/go/GoIEPRestrictionsRuleTest.java
+++ OWLTools-Annotation/src/test/java/owltools/gaf/rules/go/GoIEPRestrictionsRuleTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyIRIMapper;
 
 import owltools.OWLToolsTestBasics;
 import owltools.gaf.GafDocument;
@@ -14,6 +16,9 @@ import owltools.gaf.GafObjectsBuilder;
 import owltools.gaf.GeneAnnotation;
 import owltools.gaf.rules.AnnotationRule;
 import owltools.gaf.rules.AnnotationRuleViolation;
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.CatalogXmlIRIMapper;
+import owltools.io.ParserWrapper;
 
 /**
  * Tests for {@link GoIEPRestrictionsRule}.
@@ -24,7 +29,13 @@ public class GoIEPRestrictionsRuleTest extends OWLToolsTestBasics {
 	public void test() throws Exception {
 		GafObjectsBuilder builder = new GafObjectsBuilder();
 		GafDocument gafdoc = builder.buildDocument(getResource("test_gene_association_mgi.gaf"));
-		AnnotationRule rule = new GoIEPRestrictionsRule(getGraph("rules/taxon/gene_ontology_ext.obo"), getGraph("eco.obo"));
+		
+		ParserWrapper p = new ParserWrapper();
+		OWLOntologyIRIMapper mapper = new CatalogXmlIRIMapper(getResource("rules/ontology/extensions/catalog-v001.xml"));
+		p.addIRIMapper(mapper);
+		OWLOntology goTaxon = p.parse("http://purl.obolibrary.org/obo/go/extensions/x-taxon-importer.owl");
+		
+		AnnotationRule rule = new GoIEPRestrictionsRule(new OWLGraphWrapper(goTaxon), getGraph("eco.obo"));
 		List<GeneAnnotation> annotations = gafdoc.getGeneAnnotations();
 		
 		List<AnnotationRuleViolation> allViolations = new ArrayList<AnnotationRuleViolation>();
