42346ab18976684f70c88f9c3f425d18b530d7c6
Chris Mungall <cjm@berkeleybop.org>
2012-10-30 18:26:20 +0000

    git-svn-id: http://owltools.googlecode.com/svn/trunk@1014 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Core/src/main/java/owltools/mooncat/ontologymetadata/package-info.java
@@ -0,0 +1,17 @@
+/**
+   <h2>OntologyMetadata<h2>
+   
+   <p>
+   
+   This package contains tools for extracting metadata from ontology headers and import chains.
+   
+   See also:
+   
+   http://code.google.com/p/obo-registry/
+   
+   </p>
+   
+@author cjm
+
+ */
+package owltools.mooncat.ontologymetadata;
