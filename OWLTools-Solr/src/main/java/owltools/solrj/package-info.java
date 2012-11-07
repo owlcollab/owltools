42b6a8ab467e424ff154563d5e5a6e4e1b3fec53
sjcarbon@lbl.gov <sjcarbon@lbl.gov>
2012-11-07 01:03:19 +0000

    a little detail
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1029 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Solr/src/main/java/owltools/solrj/package-info.java
+++ OWLTools-Solr/src/main/java/owltools/solrj/package-info.java
@@ -7,7 +7,13 @@
  * 
  * <p>
  * Some of the basic groundwork is covered by {@link owltools.solrj.AbstractSolrLoader}, but the two extreme specific cases are
- * probably better examples for the possible ways of loading an index.
+ * probably better examples for the possible ways of loading an index. 
+ * </p>
+ * 
+ * <p>
+ * There are presently two file loading methods: one fairly generic one for loading ontology files 
+ * ({@link owltools.solrj.FlexSolrDocumentLoader}) that requires the use of a YAML configuration file and 
+ * one narrow one that's tied to the Gene Ontology's specific schema for GAF files ({@link owltools.solrj.GafSolrDocumentLoader}).
  * </p>
  */
 package owltools.solrj;
