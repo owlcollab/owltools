4fb7c93937d2be05ca9c9859e9063263e7400670
Heiko Dietze <hdietze@lbl.gov>
2013-01-14 22:32:19 +0000

    improve implementation of annotation rule GO_AR:0000008. Use a subset tag to identity the offending terms.
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1097 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Annotation/src/main/java/owltools/gaf/rules/go/GoNoHighLevelTermAnnotationRule.java
@@ -0,0 +1,56 @@
+package owltools.gaf.rules.go;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLClass;
+
+import owltools.gaf.GeneAnnotation;
+import owltools.gaf.rules.AbstractAnnotationRule;
+import owltools.gaf.rules.AnnotationRuleViolation;
+import owltools.graph.OWLGraphWrapper;
+
+/**
+ * GO_AR:0000008
+ */
+public class GoNoHighLevelTermAnnotationRule extends AbstractAnnotationRule {
+	
+	/**
+	 * The string to identify this class in the annotation_qc.xml and related factories.
+	 * This is not supposed to be changed. 
+	 */
+	public static final String PERMANENT_JAVA_ID = "org.geneontology.rules.GO_AR_0000008";
+	
+	private final String high_level_subset = "high_level_annotation_qc";
+	private final OWLGraphWrapper go;
+
+	/**
+	 * @param go
+	 */
+	public GoNoHighLevelTermAnnotationRule(OWLGraphWrapper go) {
+		super();
+		this.go = go;
+	}
+
+	@Override
+	public Set<AnnotationRuleViolation> getRuleViolations(GeneAnnotation a) {
+		String cls = a.getCls();
+		if (cls != null) {
+			OWLClass owlClass = go.getOWLClassByIdentifier(cls);
+			if (owlClass != null) {
+				List<String> subsets = go.getSubsets(owlClass);
+				if (subsets != null && !subsets.isEmpty()) {
+					for (String subset : subsets) {
+						if (high_level_subset.equals(subset)) {
+							String message = "Do not annotate to: "+cls+" The term is considered to high level, as marked via the subset tag: "+high_level_subset;
+							return Collections.singleton(new AnnotationRuleViolation(getRuleId(), message , a));
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+}
---
+++ OWLTools-Annotation/src/test/java/owltools/gaf/rules/go/GoNoHighLevelTermAnnotationRuleTest.java
@@ -0,0 +1,48 @@
+package owltools.gaf.rules.go;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyIRIMapper;
+
+import owltools.OWLToolsTestBasics;
+import owltools.gaf.GafDocument;
+import owltools.gaf.GafObjectsBuilder;
+import owltools.gaf.GeneAnnotation;
+import owltools.gaf.rules.AnnotationRule;
+import owltools.gaf.rules.AnnotationRuleViolation;
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.CatalogXmlIRIMapper;
+import owltools.io.ParserWrapper;
+
+public class GoNoHighLevelTermAnnotationRuleTest extends OWLToolsTestBasics {
+
+	@Test
+	public void test() throws Exception {
+		GafObjectsBuilder builder = new GafObjectsBuilder();
+		GafDocument gafdoc = builder.buildDocument(getResource("test_gene_association_mgi.gaf"));
+		
+		ParserWrapper p = new ParserWrapper();
+		OWLOntologyIRIMapper mapper = new CatalogXmlIRIMapper(getResource("rules/ontology/extensions/catalog-v001.xml"));
+		p.addIRIMapper(mapper);
+		OWLOntology go = p.parse("http://purl.obolibrary.org/obo/go.owl");
+		
+		AnnotationRule rule = new GoNoHighLevelTermAnnotationRule(new OWLGraphWrapper(go));
+		List<GeneAnnotation> annotations = gafdoc.getGeneAnnotations();
+		
+		List<AnnotationRuleViolation> allViolations = new ArrayList<AnnotationRuleViolation>();
+		for (GeneAnnotation annotation : annotations) {
+			Set<AnnotationRuleViolation> violations = rule.getRuleViolations(annotation);
+			if (violations != null && !violations.isEmpty()) {
+				allViolations.addAll(violations);
+			}
+		}
+		assertEquals(2, allViolations.size());
+	}
+
+}
