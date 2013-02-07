86aad187805b4afe99308bc00ad47adbb158e870
Chris Mungall <cjm@berkeleybop.org>
2013-02-07 03:30:26 +0000

    git-svn-id: http://owltools.googlecode.com/svn/trunk@1120 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Core/src/test/java/owltools/mooncat/PropertyExtractorTest.java
@@ -0,0 +1,68 @@
+package owltools.mooncat;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.coode.owlapi.obo.parser.OBOOntologyFormat;
+import org.junit.Test;
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.reasoner.InferenceType;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+
+import owltools.OWLToolsTestBasics;
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.OWLPrettyPrinter;
+import owltools.io.ParserWrapper;
+
+/**
+ * This is the main test class for PropertyViewOntologyBuilder
+ * 
+ * @author cjm
+ *
+ */
+public class PropertyExtractorTest extends OWLToolsTestBasics {
+
+	private static boolean RENDER_ONTOLOGY_FLAG = false;
+	
+	private Logger LOG = Logger.getLogger(PropertyExtractorTest.class);
+
+	
+	@Test
+	public void testExtractBridge() throws Exception {
+		ParserWrapper pw = new ParserWrapper();
+		
+		OWLOntology sourceOntol = pw.parseOBO(getResourceIRIString("go_xp_predictor_test_subset.obo"));
+		OWLOntology propertyOntol = pw.parseOWL(getResourceIRIString("ro.owl"));
+		PropertyExtractor pe = new PropertyExtractor(propertyOntol, sourceOntol);
+		
+		OWLOntology pont = pe.extractPropertyOntology();
+		
+		
+		pw.saveOWL(pont, new OBOOntologyFormat(), "target/foo.obo", new OWLGraphWrapper(pont));
+	}
+
+
+
+}
