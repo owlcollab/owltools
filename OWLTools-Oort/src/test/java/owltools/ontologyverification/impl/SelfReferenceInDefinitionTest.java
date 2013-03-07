d32d61a0a653f6882c9d9cf5a3e51a36ba0d5196
Heiko Dietze <hdietze@lbl.gov>
2013-03-07 21:10:39 +0000

    extend self reference check test case
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1169 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Oort/src/test/java/owltools/ontologyverification/impl/SelfReferenceInDefinitionTest.java
+++ OWLTools-Oort/src/test/java/owltools/ontologyverification/impl/SelfReferenceInDefinitionTest.java
@@ -3,6 +3,7 @@ package owltools.ontologyverification.impl;
 import static org.junit.Assert.*;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.junit.Test;
 import org.semanticweb.owlapi.model.IRI;
@@ -26,9 +27,13 @@ public class SelfReferenceInDefinitionTest extends OWLToolsTestBasics {
 		
 		OntologyCheck check = new SelfReferenceInDefinition();
 		Collection<CheckWarning> warnings = check.check(graph, graph.getAllOWLObjects());
-		assertEquals(1, warnings.size());
-		CheckWarning warning = warnings.iterator().next();
-		assertTrue(warning.getIris().contains(IRI.create("http://purl.obolibrary.org/obo/FOO_0004")));
+		assertEquals(2, warnings.size());
+		final Iterator<CheckWarning> iterator = warnings.iterator();
+		CheckWarning warning1 = iterator.next();
+		assertTrue(warning1.getIris().contains(IRI.create("http://purl.obolibrary.org/obo/FOO_0004")));
+		
+		CheckWarning warning2 = iterator.next();
+		assertTrue(warning2.getIris().contains(IRI.create("http://purl.obolibrary.org/obo/FOO_0006")));
 	}
 
 }
