11d6b09819a9d6182bd4e30b2cad55a2a4d9b51d
Heiko Dietze <hdietze@lbl.gov>
2013-03-13 18:30:56 +0000

    remove unnecessary file write in test
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1173 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Core/src/test/java/owltools/graph/ExpandRelationChainTest.java
+++ OWLTools-Core/src/test/java/owltools/graph/ExpandRelationChainTest.java
@@ -2,17 +2,12 @@ package owltools.graph;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
 import java.util.List;
 
 import org.junit.Test;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import owltools.OWLToolsTestBasics;
-import owltools.graph.OWLGraphWrapper;
 import owltools.io.ParserWrapper;
 
 public class ExpandRelationChainTest extends OWLToolsTestBasics {
@@ -23,10 +18,6 @@ public class ExpandRelationChainTest extends OWLToolsTestBasics {
 		ParserWrapper p = new ParserWrapper();
 		
 		OWLGraphWrapper graph = p.parseToOWLGraph(getResourceIRIString("graph/expand_relation_chain.obo"));
-		
-		OWLOntology ontology = graph.getSourceOntology();
-		OWLOntologyManager manager = ontology.getOWLOntologyManager();
-		manager.saveOntology(ontology, IRI.create(new File("exapnd_rel.owl")));
 		
 		List<OWLObjectProperty> relations = graph.expandRelationChain("regulates_o_has_participant");
 		
