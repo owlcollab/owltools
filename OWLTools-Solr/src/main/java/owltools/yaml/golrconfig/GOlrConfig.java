1c892b1eb217c35547d52f10ad098027f1393347
sjcarbon@lbl.gov <sjcarbon@lbl.gov>
2013-04-26 19:56:16 +0000

    add new field to schema
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1249 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Solr/src/main/java/owltools/yaml/golrconfig/GOlrConfig.java
+++ OWLTools-Solr/src/main/java/owltools/yaml/golrconfig/GOlrConfig.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class GOlrConfig {
 
 	public String id;
+	public Boolean schema_generating;
 	public String description;
 	public String display_name;
 	public String document_category;
@@ -27,4 +28,3 @@ public class GOlrConfig {
 		result_weights = "";
 	}
 }
-
