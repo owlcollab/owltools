0436b667a501adff4d078fc73f23c6671de1164f
Heiko Dietze <hdietze@lbl.gov>
2013-05-21 00:05:01 +0000

    copy to new project
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1317 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Sim/src/test/java/sim1/AbstractSimEngineTest.java
@@ -0,0 +1,83 @@
+package sim1;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.obolibrary.oboformat.parser.OBOFormatParserException;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+import owltools.OWLToolsTestBasics;
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.OWLPrettyPrinter;
+import owltools.io.ParserWrapper;
+import owltools.sim.SimEngine;
+import owltools.sim.Similarity;
+
+public abstract class AbstractSimEngineTest extends OWLToolsTestBasics {
+
+	protected void run(OWLGraphWrapper  wrapper, Similarity sim, OWLObject a, OWLObject b) throws Exception{
+		OWLPrettyPrinter pp = new OWLPrettyPrinter(wrapper);
+		SimEngine se = new SimEngine(wrapper);
+		System.out.println("Comparing "+pp.render(a)+" -vs- "+pp.render(b));
+		/*
+		for (OWLObject x : wrapper.getDescendantsReflexive(a)) {
+			System.out.println("d="+x);
+		}
+		*/
+		
+		OWLClassExpression lcs = se.getLeastCommonSubsumerSimpleClassExpression(a, b);
+		if (lcs != null) {
+			System.out.println(" LCS: "+pp.render(lcs));			
+		}
+		
+		se.calculateSimilarity(sim, a, b);
+		
+		System.out.println(sim);
+		for (OWLAxiom ax : sim.translateResultsToOWLAxioms()) {
+			System.out.println("  Ax: "+pp.render(ax));
+		}
+		
+	}
+	
+	protected void runAll(OWLGraphWrapper  wrapper, OWLObject a, OWLObject b) throws Exception{
+		OWLPrettyPrinter pp = new OWLPrettyPrinter(wrapper);
+		SimEngine se = new SimEngine(wrapper);
+		System.out.println("Comparing "+pp.render(a)+" -vs- "+pp.render(b));
+		/*
+		for (OWLObject x : wrapper.getDescendantsReflexive(a)) {
+			System.out.println("d="+x);
+		}
+		*/
+		
+		OWLClassExpression lcs = se.getLeastCommonSubsumerSimpleClassExpression(a, b);
+		if (lcs != null) {
+			System.out.println(" LCS: "+pp.render(lcs));			
+		}
+		
+		Set<Similarity> r = se.calculateAllSimilarity(a, b);
+		for (Similarity sim : r) {
+
+			System.out.println(sim);
+			for (OWLAxiom ax : sim.translateResultsToOWLAxioms()) {
+				System.out.println("  Ax: "+pp.render(ax));
+			}
+		}
+		
+	}
+
+	
+	protected OWLGraphWrapper getOntologyWrapper(String file) throws OWLOntologyCreationException, IOException, OBOFormatParserException {
+		ParserWrapper pw = new ParserWrapper();
+		return pw.parseToOWLGraph(getResourceIRIString(file));
+	}
+	
+	protected OWLGraphWrapper getOntologyWrapperFromURL(String url) throws OWLOntologyCreationException, IOException, OBOFormatParserException {
+		ParserWrapper pw = new ParserWrapper();
+		return pw.parseToOWLGraph(url);
+	}
+	
+	
+}
