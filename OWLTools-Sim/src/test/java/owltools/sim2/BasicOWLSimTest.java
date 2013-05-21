f2f9d674493feaf441f3776460d09ac6743625ed
Heiko Dietze <hdietze@lbl.gov>
2013-05-21 00:04:38 +0000

    copy to new project
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1316 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Sim/src/test/java/owltools/sim2/BasicOWLSimTest.java
@@ -0,0 +1,136 @@
+package owltools.sim2;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import org.apache.commons.math.MathException;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+
+import owltools.OWLToolsTestBasics;
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.OWLPrettyPrinter;
+import owltools.io.ParserWrapper;
+import owltools.io.TableToAxiomConverter;
+import owltools.sim2.SimpleOwlSim;
+import owltools.sim2.SimpleOwlSim.ScoreAttributesPair;
+import owltools.sim2.preprocessor.NullSimPreProcessor;
+import owltools.sim2.preprocessor.PhenoSimHQEPreProcessor;
+import owltools.sim2.preprocessor.SimPreProcessor;
+
+/**
+ * This is the main test class for PropertyViewOntologyBuilder
+ * 
+ * @author cjm
+ *
+ */
+public class BasicOWLSimTest extends OWLToolsTestBasics {
+
+	private Logger LOG = Logger.getLogger(BasicOWLSimTest.class);
+	OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+	OWLDataFactory df = manager.getOWLDataFactory();
+	OWLOntology sourceOntol;
+	SimPreProcessor pproc;
+	OWLPrettyPrinter owlpp;
+	OWLGraphWrapper g;
+	SimpleOwlSim sos;
+
+	@Test
+	public void testPhenoSim() throws IOException, OWLOntologyCreationException, OWLOntologyStorageException, MathException {
+		ParserWrapper pw = new ParserWrapper();
+		sourceOntol = pw.parseOWL(getResourceIRIString("sim/mp-subset-1.obo"));
+		g =  new OWLGraphWrapper(sourceOntol);
+		parseAssociations(getResource("sim/mgi-gene2mp-subset-1.tbl"), g);
+
+		owlpp = new OWLPrettyPrinter(g);
+
+		// assume buffering
+		OWLReasoner reasoner = new ElkReasonerFactory().createReasoner(sourceOntol);
+		try {
+			pproc = new NullSimPreProcessor();
+			pproc.setInputOntology(sourceOntol);
+			pproc.setOutputOntology(sourceOntol);
+			pproc.setReasoner(reasoner);
+			pproc.setOWLPrettyPrinter(owlpp);
+
+
+			sos = new SimpleOwlSim(sourceOntol);
+			sos.setSimPreProcessor(pproc);
+			sos.createElementAttributeMapFromOntology();
+
+			//sos.saveOntology("/tmp/z.owl");
+
+			reasoner.flush();
+			for (OWLNamedIndividual i : sourceOntol.getIndividualsInSignature()) {
+				//System.out.println("COMPARING: "+i);
+				for (OWLNamedIndividual j : sourceOntol.getIndividualsInSignature()) {
+					showSim(i,j);
+				}
+			}
+		}
+		finally {
+			reasoner.dispose();
+		}
+	}
+
+	protected void parseAssociations(File file, OWLGraphWrapper g) throws IOException {
+		TableToAxiomConverter ttac = new TableToAxiomConverter(g);
+		ttac.config.axiomType = AxiomType.CLASS_ASSERTION;
+		ttac.config.isSwitchSubjectObject = true;
+		ttac.parse(file);			
+	}
+
+
+
+	private void showSim(OWLNamedIndividual i, OWLNamedIndividual j) {
+		
+		if (i==j) return;
+		float s = sos.getElementJaccardSimilarity(i, j);
+		if (s > 0.1) {
+			LOG.info("SimJ( "+i+" , "+j+" ) = "+s);
+
+			ScoreAttributesPair maxic = sos.getSimilarityMaxIC(i, j);
+			LOG.info("MaxIC( "+i+" , "+j+" ) = "+maxic.score+" "+show(maxic.attributeClassSet));
+
+			ScoreAttributesPair bma = sos.getSimilarityBestMatchAverageAsym(i, j);
+			LOG.info("BMAasym( "+i+" , "+j+" ) = "+bma.score+" "+show(bma.attributeClassSet));
+		}
+
+	}
+
+
+
+
+	private String show(Set<OWLClassExpression> cset) {
+		StringBuffer sb = new StringBuffer();
+		for (OWLClassExpression c : cset) {
+			sb.append(owlpp.render(c) + " ; ");
+		}
+		return sb.toString();
+	}
+
+	private OWLClass get(String iri) {
+		return df.getOWLClass(IRI.create("http://x.org#"+iri));
+	}
+
+
+
+
+
+}
---
+++ OWLTools-Sim/src/test/java/owltools/sim2/EnrichmentTest.java
@@ -0,0 +1,131 @@
+package owltools.sim2;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.apache.commons.math.MathException;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+
+import owltools.OWLToolsTestBasics;
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.OWLPrettyPrinter;
+import owltools.io.ParserWrapper;
+import owltools.io.TableToAxiomConverter;
+import owltools.sim2.SimpleOwlSim;
+import owltools.sim2.SimpleOwlSim.EnrichmentConfig;
+import owltools.sim2.SimpleOwlSim.EnrichmentResult;
+import owltools.sim2.preprocessor.AutomaticSimPreProcessor;
+
+/**
+ * This is the main test class for PropertyViewOntologyBuilder
+ * 
+ * @author cjm
+ *
+ */
+public class EnrichmentTest extends OWLToolsTestBasics {
+
+	private Logger LOG = Logger.getLogger(EnrichmentTest.class);
+	OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+	OWLDataFactory df = manager.getOWLDataFactory();
+	OWLOntology sourceOntol;
+	SimpleOwlSim sos;
+	OWLGraphWrapper g;
+
+
+
+	@Test
+	public void enrichmentTestGO() throws Exception, MathException {
+		ParserWrapper pw = new ParserWrapper();
+		sourceOntol = pw.parseOBO(getResourceIRIString("go-subset-t1.obo"));
+		g = new OWLGraphWrapper(sourceOntol);
+		IRI vpIRI = g.getOWLObjectPropertyByIdentifier("GOTESTREL:0000001").getIRI();
+		TableToAxiomConverter ttac = new TableToAxiomConverter(g);
+		ttac.config.axiomType = AxiomType.CLASS_ASSERTION;
+		ttac.config.property = vpIRI;
+		ttac.config.isSwitchSubjectObject = true;
+		ttac.parse("src/test/resources/simplegaf-t1.txt");
+		// assume buffering
+		OWLReasoner reasoner = new ElkReasonerFactory().createReasoner(sourceOntol);
+		try {
+			OWLPrettyPrinter pp = new OWLPrettyPrinter(g);
+
+			sos = new SimpleOwlSim(sourceOntol);
+
+			AutomaticSimPreProcessor pproc = new AutomaticSimPreProcessor();
+			pproc.setInputOntology(sourceOntol);
+			pproc.setOutputOntology(sourceOntol);
+			pproc.setReasoner(reasoner); // TODO - share
+
+			sos.setSimPreProcessor(pproc);
+			//sos.preprocess();
+			pproc.preprocess();
+
+			sos.createElementAttributeMapFromOntology();
+
+			for (OWLNamedIndividual ind : sourceOntol.getIndividualsInSignature()) {
+				System.out.println(ind);
+				for (OWLClass c : reasoner.getTypes(ind, true).getFlattened()) {
+					System.out.println("  T:"+c);
+				}
+			}
+			//System.exit(0);
+
+			//sos.addViewProperty(vpIRI);
+			//sos.generatePropertyViews();
+			//sos.saveOntology("/tmp/foo.owl");
+
+
+			OWLClass rc1 = get("biological_process");
+			OWLClass rc2 = get("cellular_component");
+			OWLClass pc = g.getDataFactory().getOWLThing();
+
+			EnrichmentConfig ec = new EnrichmentConfig();
+			ec.pValueCorrectedCutoff = 0.05;
+			ec.attributeInformationContentCutoff = 3.0;
+			sos.setEnrichmentConfig(ec);
+
+			int n = 0;
+			for (OWLClass vrc1 : pproc.getViewClasses(rc1)) {
+				for (OWLClass vrc2 : pproc.getViewClasses(rc2)) {
+					List<EnrichmentResult> results = sos.calculateAllByAllEnrichment(pc, vrc1, vrc2);
+					System.out.println("Results: "+vrc1+" "+vrc2);
+					for (EnrichmentResult result : results) {
+						System.out.println(render(result,pp));
+						n++;
+					}
+				}
+			}
+			assertTrue(n > 0);
+		}
+		finally {
+			reasoner.dispose();
+		}
+
+	}
+
+	private String render(EnrichmentResult r, OWLPrettyPrinter pp) {
+		return pp.render(r.sampleSetClass) +" "+ pp.render(r.enrichedClass)
+		+" "+ r.pValue +" "+ r.pValueCorrected;
+	}
+
+	private OWLClass get(String label) {
+		return (OWLClass)g.getOWLObjectByLabel(label);
+	}
+
+
+
+
+
+}
