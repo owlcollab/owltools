382085748371f48061a10de137f83cb56f64875a
Heiko Dietze <hdietze@lbl.gov>
2013-05-28 23:45:52 +0000

    use auto-generated hashcode and equals methods
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1370 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Sim/src/main/java/owltools/sim2/ClassExpressionPair.java
+++ OWLTools-Sim/src/main/java/owltools/sim2/ClassExpressionPair.java
@@ -5,25 +5,42 @@ import org.semanticweb.owlapi.model.OWLClassExpression;
 public class ClassExpressionPair {
 	public OWLClassExpression c1;
 	public OWLClassExpression c2;
-	public ClassExpressionPair(OWLClassExpression c1,
-			OWLClassExpression c2) {
+	
+	public ClassExpressionPair(OWLClassExpression c1, OWLClassExpression c2) {
 		super();
 		this.c1 = c1;
 		this.c2 = c2;
 	}
+	
+	@Override
 	public int hashCode() {
-		int n1 = c1.hashCode();
-		int n2 = c2.hashCode();
-
-		return (n1 + n2) * n2 + n1;
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((c1 == null) ? 0 : c1.hashCode());
+		result = prime * result + ((c2 == null) ? 0 : c2.hashCode());
+		return result;
 	}
-	public boolean equals(Object other) {
-		if (other instanceof ClassExpressionPair) {
-			ClassExpressionPair otherPair = (ClassExpressionPair) other;
-			return this.c1 == otherPair.c1 && this.c2 == otherPair.c2;
-		}
-
-		return false;
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClassExpressionPair other = (ClassExpressionPair) obj;
+		if (c1 == null) {
+			if (other.c1 != null)
+				return false;
+		} else if (!c1.equals(other.c1))
+			return false;
+		if (c2 == null) {
+			if (other.c2 != null)
+				return false;
+		} else if (!c2.equals(other.c2))
+			return false;
+		return true;
 	}
 
 }
