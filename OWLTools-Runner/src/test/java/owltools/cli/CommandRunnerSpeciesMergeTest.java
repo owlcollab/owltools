506e367b55cfc7e7fcbbb2a420c67e34a6307384
Chris Mungall <cjm@berkeleybop.org>
2013-06-21 17:48:49 +0000

    new test for --merge-species command
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1421 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Runner/src/test/java/owltools/cli/CommandRunnerSpeciesMergeTest.java
@@ -0,0 +1,44 @@
+package owltools.cli;
+
+import static junit.framework.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import owltools.OWLToolsTestBasics;
+
+/**
+ * Tests for {@link CommandRunner}.
+ * 
+ * these are somewhat ad-hoc at the moment - output is written to stdout;
+ * no attempt made to check results
+ * 
+ */
+public class CommandRunnerSpeciesMergeTest extends AbstractCommandRunnerTest {
+	
+	@Test
+	public void testMMergeSpecies() throws Exception {
+		init();
+		//String obo = "http://purl.obolibrary.org/obo";
+		load("forebrain.obo");
+		//run("--list-class-axioms NCBITaxon:7954");
+		
+		run("--reasoner elk --merge-species-ontology -s Danio -t NCBITaxon:7954");
+		run("-o -f obo target/forebrain-merged.obo");
+		run("--assert-inferred-subclass-axioms --removeRedundant");
+		run("-o -f obo target/forebrain-merged-trimmed.obo");
+		run("--descendants telencephalon");
+		//run("-a tentacle");
+
+		//run("--make-subset-by-properties part_of // -o -f obo /tmp/foo.obo");
+		
+		//run("-o -f obo /tmp/foo.obo");
+		
+	}
+	
+
+
+	
+	
+}
