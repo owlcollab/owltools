bef4a9b077db15053271ed3284cc99a55499108e
Chris Mungall <cjm@berkeleybop.org>
2013-10-22 02:52:40 +0000

    docs
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1663 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Sim/src/main/java/owltools/sim2/SimpleOwlSim.java
+++ OWLTools-Sim/src/main/java/owltools/sim2/SimpleOwlSim.java
@@ -47,7 +47,18 @@ import owltools.util.ClassExpressionPair;
  * OWLIndividuals) to another set, based on the attributes they possess (modeled
  * as OWLClasses).
  * 
- * <h3>Pre-processing</h3>
+ * Entities may be genes, genotypes, diseases, organisms, ...
+ * 
+ * Attributes may be functions, phenotypes, ...
+ * 
+ * Usage is generic beyond biology. The entities compared could be pizzas,
+ * and the attrubutes could be toppings.
+ * 
+ * Similarity scores are generated for any pair of entities based on
+ * shared attributes. An <i>ontology</i> is used so that we can compare based
+ * on shared <i>inferred</i> attributes as well.
+ * 
+ * <h3>Pre-processing (optional)</h3>
  * 
  * Each individual is assumed to be an instance of the class used in the
  * comparison. This may not be the natural representation, so some
@@ -91,8 +102,34 @@ import owltools.util.ClassExpressionPair;
  * 
  * <h3>Reasoning</h3>
  * 
- * Any reasoner can be plugged in but for most datasets we use Elk 0.3, which
- * can scale well
+ * Any reasoner can be plugged in but for most datasets we use Elk 0.4.x, which
+ * can scale well.
+ * 
+ * A reasoner performs operations over an ontology consisting of OWLIndividuals and OWLClasses.
+ * We typically use i, j for variable names for instances, and a, b, c, d as variable names
+ * for classes.
+ * 
+ * The following operations are used:
+ * 
+ * <ul>
+ * <li> <i>Type(i)</i> : {@link OWLReasoner#getTypes(OWLNamedIndividual, boolean)} - to find all of the attributes possessed
+ * by an entity - may be direct or indirect. The inverse operation is <i>Inst(c)</i>
+ * <li> <i>Sub(c)</i> :  {@link OWLReasoner#getSuperClasses(OWLClassExpression, boolean)} - to find all the ancestors (subsumers) of an attribute - direct and indirect.
+ * The inverse operation is <i>SubBy(c)</i>
+ * <li> <i>Eq(c)</i> : {@link OWLReasoner#getEquivalentClasses(OWLClassExpression)} - find all all equivalent attributes/
+ * Note that OWLSim is cycle-safe - cycles over SubClassOf are permitted, these are the same as an equivalence axiom between the classes
+ * </ul>
+ * 
+ * The latter two are combined to yield the <i>reflexive</i> common subsumers list - 
+ * see {@link #getNamedReflexiveSubsumers(OWLClassExpression)}. We write this here as
+ * RSub(c). i.e.
+ * <pre>
+ * RSub(c) = Sub(c) &cup; Eq(c)
+ * </pre>
+ * 
+ * The use of an OWLReasoner could be replaced with a basic graph traversal algorithm,
+ * but it's useful to use a reasoner here, as the ontology may not have been <i>pre-reasoned</i>
+ * 	
  * 
  * <h3>Similarity metrics</h3>
  * 
@@ -100,15 +137,71 @@ import owltools.util.ClassExpressionPair;
  * comparison
  * 
  * <ul>
- * <li>Between two individuals (e.g. between two genes, two organisms, or a
+ * <li>Between two <i>classes</i> (aka attributes, which are used to describe individuals)
+ * <li>Between two <i>individuals</i> (aka entities, e.g. between two genes, two organisms, or a
  * disease vs genotype)
- * <li>Between two classes (which are used to describe individuals)
  * </ul>
  * 
- * class-class comparisons are based on inferred common subsumers
+ * The former can be used to calculate the latter.
  * 
- * individual-individual comparisons are based on classes instantiated by both
+ * <h4>Common Subsumers and Lowest Common Subsumers</h4>
  * 
+ * class-class comparisons are based on <i>inferred common subsumers</i>.
+ * <pre>
+ * CS(c,d) = RSub(c) &cup; RSub(d)
+ * </pre>
+ * 
+ * The <i>Lowest Common Subsumer</i> is the set of common subsumers that are not
+ * redundant with other common subsumers:
+ * 
+ * <pre>
+ * LCS(c,d) = { a : a &in; CS(c,d), not [ &E; a' : a' &in; CS(c,d), c' in Sub(c) ] }
+ * </pre>
+ * 
+ * This is implemented by {@link #getLowestCommonSubsumerIC(OWLClassExpression, OWLClassExpression)}
+ * 
+ * <h4>Information Content</h4>
+ * 
+ * The <i>information content</i> (IC) of an class is a measure of how rare the class is
+ * 
+ *  <pre>
+ *  IC(c) = -log<sub>2</sub>( Pr(c) )
+ *  </pre>
+ *  
+ *  Where the probability Pr of a class c is the frequency of that class (number of
+ *  instances that instantiate it) divided by the number of instances in the corpus:
+ *  
+ *  <pre>
+ *  Pr(c) = | Inst(c) | / | Inst(RootClass) |
+ *  </pre>
+ * 
+ *  Here the semantics of Inst(c) are provided by
+ *   {@link OWLReasoner#getInstances(OWLClassExpression, boolean)}
+ *  
+ *  <h4>Similarity between classes</h4>
+ *  
+ *  One measure of similarity between two classes is the IC of the lowest common
+ *  subsumer. See {@link #getLowestCommonSubsumerIC(OWLClassExpression, OWLClassExpression)}
+ *  
+ *  <h4>Similarity Measures between Entities (Individuals)</h4>
+ *  
+ *  There are a variety of methods for comparing two entities based on the
+ *  attributes they share.
+ *  
+ *  Some methods take the two best-mtaching attributes, others compute some kind of
+ *  sum of similarity over the ensemble of attributes.
+ *  
+ *  An example of a best-attribute method is the <i>maximum information content
+ *  of the lowest common subsumers</i>. This is implemented by
+ *  {@link #getSimilarityMaxIC(OWLNamedIndividual, OWLNamedIndividual)}. This
+ *  is the same method as described in Lord et al 2003.
+ *  
+ *  Other methods
+ *  <ul>
+ *  <li> {@link #getElementJaccardSimilarity(OWLNamedIndividual, OWLNamedIndividual)}
+ *  <li> {@link #getElementGraphInformationContentSimilarity(OWLNamedIndividual, OWLNamedIndividual)}
+ *  <li> 
+ *  </ul>
  * 
  * @author cjm
  * 
@@ -483,6 +576,9 @@ public class SimpleOwlSim {
 	 * @return nodes for all classes that a instantiates - direct and inferred
 	 */
 	public Set<Node<OWLClass>> getInferredAttributes(OWLNamedIndividual a) {
+		// TODO - refactor this method - in previous versions of Elk it was
+		// not possible to ask for the types an instance instantiates, now
+		// with Elk 0.4 it is
 		if (elementToInferredAttributesMap.containsKey(a))
 			return new HashSet<Node<OWLClass>>(elementToInferredAttributesMap.get(a));
 		Set<Node<OWLClass>> nodes = new HashSet<Node<OWLClass>>();
@@ -523,11 +619,13 @@ public class SimpleOwlSim {
 	}
 
 	/**
-	 * NOT CACHED
 	 * 
 	 * <pre>
-	 *   CS(a,b) = { c : c &isin; anc(a), c &isin; anc(b) }
+	 *   CS(a,b) = { c : c &isin; RSub(a), c &isin; RSub(b) }
 	 * </pre>
+	 * 
+	 * NOT CACHED.
+	 * Always returns a copy of the CS list
 	 * 
 	 * @param a
 	 * @param b
@@ -546,6 +644,15 @@ public class SimpleOwlSim {
 		return new HashSet<Node<OWLClass>>(nodes);
 	}
 
+	/**
+	 * <pre>
+	 *   CS(i,j) = { c : c &isin; Type(i), c &isin; Type(j) }
+	 * </pre>
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public Set<Node<OWLClass>> getNamedCommonSubsumers(OWLNamedIndividual a,
 			OWLNamedIndividual b) {
 		// we don't cache this as we assume it will be called at most once
@@ -556,13 +663,13 @@ public class SimpleOwlSim {
 
 	/**
 	 * <pre>
-	 *   CS<SUB>redundant</SUB> = { c : d &isin; CS(a,b), d non-reflexive-SubClassOf c }
+	 *   CS<sub>redundant</sub>(a,b) = { c : c &in; CS(a,b), &E; c' : c' &in; CS(a,b), c' &in Sub(c) }
 	 *   LCS(a,b) = CS(a,b) - CS<SUB>redundant</SUB>
 	 * </pre>
 	 * 
 	 * @param a
 	 * @param b
-	 * @return nodes
+	 * @return Lowest Common Subsumers of a and b
 	 */
 	public Set<Node<OWLClass>> getNamedLowestCommonSubsumers(
 			OWLClassExpression a, OWLClassExpression b) {
@@ -579,6 +686,9 @@ public class SimpleOwlSim {
 	}
 
 	/**
+	 * This method provides a generic wrapper onto other attribute-based similarity
+	 * methods
+	 * 
 	 * Compares two classes (attributes) according to the specified metric
 	 * 
 	 * @param a
@@ -614,7 +724,7 @@ public class SimpleOwlSim {
 
 	/**
 	 * <pre>
-	 * | anc(a) &cap; anc(b) | / | anc(a) &cup; anc(b) |
+	 * SimJ(a,b) = | anc(a) &cap; anc(b) | / | anc(a) &cup; anc(b) |
 	 * </pre>
 	 * 
 	 * @param a
@@ -631,8 +741,11 @@ public class SimpleOwlSim {
 
 	/**
 	 * <pre>
-	 * | T(a) &cap; T(b) | / | T(a) &cup; T(b) |
+	 * SimJ(i,j) = | Type(i) &cap; Type(j) | / | Type(i) &cup; Type(j) |
 	 * </pre>
+	 * 
+	 * Here Type(i) is the set of all (direct and indirect) inferred types
+	 * for an individual.
 	 * 
 	 * @param i
 	 * @param j
@@ -648,11 +761,16 @@ public class SimpleOwlSim {
 
 	/**
 	 * <pre>
-	 * | anc(a) &cap; anc(b) | / | anc(a)  |
+	 * SimJ<sup>ASym</sup>(c,d) = | anc(c) &cap; anc(d) | / | anc(c)  |
 	 * </pre>
 	 * 
-	 * @param a
-	 * @param b
+	 * If d is an ancestor of c then the score is 1. i.e.
+	 * <pre>
+	 * d &in; Sub(c) &rarr;  SimJ<sup>ASym</sup>(c,d) = 1
+	 * <pre>
+	 * 
+	 * @param c
+	 * @param d
 	 * @return Asymmetric SimJ of two attribute classes
 	 */
 	public double getAsymmerticAttributeJaccardSimilarity(OWLClassExpression a,
@@ -691,12 +809,13 @@ public class SimpleOwlSim {
 	}
 
 	/**
-	 * CACHED
 	 * 
 	 * <pre>
-	 *    IC(c) = -log<SUB>2</SUB>p(c)
-	 *    p(c) = | I(c) | / | I(thing) |
+	 *    IC(c) = -log<SUB>2</SUB>(p(c))
+	 *    p(c) = | Inst(c) | / | Inst(owl:Thing) |
 	 * </pre>
+	 * 
+	 * CACHED
 	 * 
 	 * @param a
 	 * @param b
@@ -769,7 +888,16 @@ public class SimpleOwlSim {
 
 	/**
 	 * Find the inferred attribute shared by both i and j that has highest IC. If
-	 * there is a tie then the resulting structure will have multiple classes
+	 * there is a tie then the resulting structure will have multiple classes.
+	 * 
+	 * <pre>
+	 * MaxIC(i,j) = max { IC(c) : c &in LCS(i,j) }
+	 * </pre>
+	 * 
+	 * As a convenience, this method also returns the LCS class as well as the IC
+	 * 
+	 * This is the same metric used by Lord et al in <a href="http://bioinformatics.oxfordjournals.org/cgi/reprint/19/10/1275">
+	 * Investigating semantic similarity measures  across the Gene Ontology: the relationship between sequence and annotation</a>.
 	 * 
 	 * @param i
 	 * @param j
@@ -795,18 +923,24 @@ public class SimpleOwlSim {
 		return best;
 	}
 
-	/**
-	 * Pesquita et al
-	 * 
-	 * @param i
-	 * @param j
-	 * @return pair
-	 */
 	public ScoreAttributesPair getSimilarityBestMatchAverageAsym(
 			OWLNamedIndividual i, OWLNamedIndividual j) {
 		return getSimilarityBestMatchAverage(i, j, Metric.IC_MCS, Direction.A_TO_B);
 	}
 
+	/**
+	 * See: Pesquita et al
+	 * 
+	 * <pre>
+	 * BMA<sup>M</sup>(i,j) = avg { s : c &in; Attr(i), max { s : d &in; Attr(j), s = M(c,d) } }
+	 * </pre>
+	 * 
+	 * 
+	 * @param i
+	 * @param j
+	 * @param metric
+	 * @return pair
+	 */
 	public ScoreAttributesPair getSimilarityBestMatchAverageAsym(OWLNamedIndividual i, OWLNamedIndividual j, Metric metric) {
 		return getSimilarityBestMatchAverage(i, j, metric, Direction.A_TO_B);
 	}
@@ -940,6 +1074,8 @@ public class SimpleOwlSim {
 
 	private void addEnrichmentResult(EnrichmentResult result,
 			List<EnrichmentResult> results) {
+		if (result == null)
+			return;
 		if (enrichmentConfig != null) {
 			if (enrichmentConfig.pValueCorrectedCutoff != null
 					&& result.pValueCorrected > enrichmentConfig.pValueCorrectedCutoff) {
@@ -951,15 +1087,16 @@ public class SimpleOwlSim {
 			}
 
 		}
+		LOG.info(result);
 		results.add(result);
 	}
 
 	/**
 	 * @param populationClass
 	 * @param pc1
-	 *          - sample set class
+	 *          - sample set root class
 	 * @param pc2
-	 *          - enriched set class
+	 *          - enriched set root class
 	 * @return enrichment results
 	 * @throws MathException
 	 */
@@ -971,11 +1108,16 @@ public class SimpleOwlSim {
 		for (OWLClass sampleSetClass : getReasoner().getSubClasses(pc1, false)
 				.getFlattened()) {
 			if (sampleSetClass.equals(nothing)) continue;
-			LOG.info("sample set class:" + sampleSetClass);
+			int sampleSetSize = getNumElementsForAttribute(sampleSetClass);
+			LOG.info("sample set class:" + sampleSetClass + " size="+sampleSetSize);
+			if (sampleSetSize < 2)
+				continue;
 			List<EnrichmentResult> resultsInner = new Vector<EnrichmentResult>();
 			for (OWLClass enrichedClass : this.getReasoner()
 					.getSubClasses(pc2, false).getFlattened()) {
 				if (enrichedClass.equals(nothing)) continue;
+				if (getNumElementsForAttribute(enrichedClass) < 2)
+					continue;
 				if (sampleSetClass.equals(enrichedClass)
 						|| this.getNamedSubsumers(enrichedClass).contains(sampleSetClass)
 						|| this.getNamedSubsumers(sampleSetClass).contains(enrichedClass)) {
@@ -1026,6 +1168,12 @@ public class SimpleOwlSim {
 		int enrichedClassSize = getNumElementsForAttribute(enrichedClass);
 		// LOG.info("Hyper :"+populationClassSize
 		// +" "+sampleSetClassSize+" "+enrichedClassSize);
+		Set<OWLNamedIndividual> eiSet = getElementsForAttribute(sampleSetClass);
+		eiSet.retainAll(this.getElementsForAttribute(enrichedClass));
+		if (eiSet.size() == 0) {
+			return null;
+		}
+		LOG.info(" shared elements: "+eiSet.size()+" for "+enrichedClass);
 		HypergeometricDistributionImpl hg = new HypergeometricDistributionImpl(
 				populationClassSize, sampleSetClassSize, enrichedClassSize);
 		/*
@@ -1033,8 +1181,6 @@ public class SimpleOwlSim {
 		 * LOG.info("sampleSetSize="+getNumElementsForAttribute(sampleSetClass));
 		 * LOG.info("enrichedClass="+getNumElementsForAttribute(enrichedClass));
 		 */
-		Set<OWLNamedIndividual> eiSet = getElementsForAttribute(sampleSetClass);
-		eiSet.retainAll(this.getElementsForAttribute(enrichedClass));
 		// LOG.info("both="+eiSet.size());
 		double p = hg.cumulativeProbability(eiSet.size(),
 				Math.min(sampleSetClassSize, enrichedClassSize));
@@ -1257,7 +1403,7 @@ public class SimpleOwlSim {
 	// IC = 2.0 : 25% (1/4)
 	// IC = 3.0 : 12.5% (1/8)
 	/**
-	 * IC = -(log {@link getNumElementsForAttribute} / corpus size) / log(2)
+	 * IC = -(log {@link #getNumElementsForAttribute(c)} / corpus size) / log(2)
 	 * @param c
 	 * @return Information Content value for a given class
 	 */
