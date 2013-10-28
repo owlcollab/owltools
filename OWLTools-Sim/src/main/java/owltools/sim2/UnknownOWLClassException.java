e2368955283c499b354ed03b9646dfa65d301bef
Chris Mungall <cjm@berkeleybop.org>
2013-10-28 22:09:21 +0000

    git-svn-id: http://owltools.googlecode.com/svn/trunk@1678 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Sim/src/main/java/owltools/sim2/UnknownOWLClassException.java
@@ -0,0 +1,11 @@
+package owltools.sim2;
+
+import org.semanticweb.owlapi.model.OWLClass;
+
+public class UnknownOWLClassException extends Exception {
+
+	public UnknownOWLClassException(OWLClass ac) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
