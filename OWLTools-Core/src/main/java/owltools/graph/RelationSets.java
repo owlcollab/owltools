216f24a13e0d7bf08c4e82cf93c046c11e7c74d1
sjcarbon@lbl.gov <sjcarbon@lbl.gov>
2013-10-30 00:29:56 +0000

    forgot a file from the last commit--sorry Jenkins
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1691 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Core/src/main/java/owltools/graph/RelationSets.java
@@ -0,0 +1,40 @@
+package owltools.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RelationSets {
+
+    public static final String ISA_PARTOF = "is_a/part_of";
+    public static final String REGULATES = "regulates/is_a/part_of";
+
+    /*
+     * Should be using this in most cases.
+     */
+    public static final String COMMON = "regulates/is_a/part_of/occurs_in";
+
+	/*
+	 * 
+	 */
+	public static List<String> getRelationSet(String relset) {
+
+		List<String> rel_ids = new ArrayList<String>();
+		if( relset != null ){
+			if( relset.equals(ISA_PARTOF) ){
+				rel_ids.add("BFO:0000050");
+			}else if( relset.equals(REGULATES) ){
+				rel_ids.add("BFO:0000050");
+				rel_ids.add("RO:0002211");
+				rel_ids.add("RO:0002212");
+				rel_ids.add("RO:0002213");
+			}else if( relset.equals(COMMON) ){
+				rel_ids.add("BFO:0000050");
+				rel_ids.add("BFO:0000066");
+				rel_ids.add("RO:0002211");
+				rel_ids.add("RO:0002212");
+				rel_ids.add("RO:0002213");
+			}
+		}
+		return rel_ids;		
+	}
+}
