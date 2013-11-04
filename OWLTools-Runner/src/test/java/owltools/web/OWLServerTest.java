a410974e539c74b11ef1c3696db4b0c577f291d5
Heiko Dietze <hdietze@lbl.gov>
2013-11-04 19:11:04 +0000

    add minimal test example for the owltools server
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1718 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Runner/src/test/java/owltools/web/OWLServerTest.java
@@ -0,0 +1,113 @@
+package owltools.web;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.eclipse.jetty.server.Server;
+import org.junit.Test;
+
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.ParserWrapper;
+import owltools.sim2.OwlSim;
+import owltools.sim2.OwlSimFactory;
+import owltools.sim2.SimpleOwlSim;
+import owltools.sim2.SimpleOwlSimFactory;
+import owltools.sim2.preprocessor.ABoxUtils;
+import owltools.sim2.preprocessor.NullSimPreProcessor;
+import owltools.sim2.preprocessor.SimPreProcessor;
+
+/**
+ * Simple test for the client server communication for the OWLTools web server.
+ */
+public class OWLServerTest {
+
+	@Test
+	public void testServerCommunication() throws Exception {
+		OWLGraphWrapper g = loadOntology("../OWLTools-Sim/src/test/resources/sim/mp-subset-1.obo");
+		
+		// set sim
+		OwlSimFactory owlSimFactory = new SimpleOwlSimFactory();
+		OwlSim sos = owlSimFactory.createOwlSim(g.getSourceOntology());
+		SimPreProcessor pproc = new NullSimPreProcessor();
+		pproc.setInputOntology(g.getSourceOntology());
+		pproc.setOutputOntology(g.getSourceOntology());
+		if (sos instanceof SimpleOwlSim)
+			((SimpleOwlSim) sos).setSimPreProcessor(pproc);
+		sos.createElementAttributeMapFromOntology();
+		// TODO	attributeAllByAllOld(opts);
+		
+		// create server
+		Server server = new Server(9031);
+		server.setHandler(new OWLServer(g, sos));
+		try {
+			server.start();
+
+			// create a client
+			HttpClient httpclient = new DefaultHttpClient();
+
+			// prepare a request
+			final HttpUriRequest httpUriRequest = createRequest();
+
+			// run request
+			HttpResponse response = httpclient.execute(httpUriRequest);
+			
+			// check response
+			HttpEntity entity = response.getEntity();
+			StatusLine statusLine = response.getStatusLine();
+			if (statusLine.getStatusCode() == 200) {
+				String responseContent = EntityUtils.toString(entity);
+				handleResponse(responseContent);
+			}
+			else {
+				EntityUtils.consumeQuietly(entity);
+			}
+
+		}
+		finally {
+			// clean up
+			server.stop();
+
+			if (pproc != null) {
+				pproc.dispose();
+			}
+		}
+	}
+
+	protected OWLGraphWrapper loadOntology(String location) throws Exception {
+		// load server ontology
+		ParserWrapper pw = new ParserWrapper();
+		OWLGraphWrapper g = pw.parseToOWLGraph(location);
+		
+		// prepare ontology
+		ABoxUtils.makeDefaultIndividuals(g.getSourceOntology());
+		return g;
+	}
+
+	protected HttpUriRequest createRequest() throws URISyntaxException {
+		URI uri = new URIBuilder()
+			.setScheme("http")
+			.setHost("localhost").setPort(9031)
+			.setPath("/")
+			.setParameter("a", "MP:0000001")
+			.setParameter("b", "MP:0000003")
+			.build();
+		HttpUriRequest httpUriRequest = new HttpGet(uri);
+		return httpUriRequest;
+	}
+
+	/**
+	 * @param responseContent
+	 */
+	protected void handleResponse(String responseContent) {
+		System.out.println(responseContent);
+	}
+}
