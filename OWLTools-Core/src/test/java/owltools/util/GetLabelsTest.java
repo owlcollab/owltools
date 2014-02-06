5c2135ce785956cb85ec9ed55593f04dc412e21c
Chris Mungall <cjm@berkeleybop.org>
2014-02-06 21:06:57 +0000

    Added example for how to fetch labels
    git-svn-id: http://owltools.googlecode.com/svn/trunk@1973 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Core/src/test/java/owltools/util/GetLabelsTest.java
@@ -0,0 +1,82 @@
+package owltools.util;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationValue;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLNamedObject;
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLPropertyExpression;
+
+import owltools.OWLToolsTestBasics;
+import owltools.graph.OWLGraphWrapper;
+import owltools.graph.shunt.OWLShuntEdge;
+import owltools.graph.shunt.OWLShuntGraph;
+import owltools.graph.shunt.OWLShuntNode;
+import owltools.io.CatalogXmlIRIMapper;
+import owltools.util.ShuntGraphUtils.ShuntGraphPair;
+import owltools.vocab.OBOUpperVocabulary;
+
+/**
+ * This test is primarily for illustrative purposes - it
+ * shows how to fetch a label for a class using the OWLAPI
+ * 
+ * @author cjm
+ *
+ */
+public class GetLabelsTest extends OWLToolsTestBasics {
+
+	OWLOntology ont;
+	
+	public class MultiLabelException extends Exception {
+
+		public MultiLabelException(OWLNamedObject obj) {
+			super("Object has multiple labels: "+obj.toString());
+		}
+
+	}
+
+	
+	@Test
+	public void testGetLabels() throws Exception {
+		OWLOntologyManager m = OWLManager.createOWLOntologyManager();
+		ont = m.loadOntologyFromOntologyDocument(getResource("caro.owl"));
+	
+		boolean allHaveLabels = true;
+		for (OWLClass c : ont.getClassesInSignature()) {
+			String label = getLabel(c);
+			if (label == null)
+				allHaveLabels = false;
+			//System.out.println(c + " LABEL:" + label);
+		}
+		assertTrue(allHaveLabels);
+	}
+	
+	private String getLabel(OWLEntity obj) throws MultiLabelException {
+		String label = null;
+		for ( OWLAnnotation ann : obj.getAnnotations(ont) ) {
+			if (ann.getProperty().isLabel()) {
+				OWLAnnotationValue v = ann.getValue();
+				if (v instanceof OWLLiteral) {
+					if (label != null) {
+						throw new MultiLabelException(obj);
+					}
+					label = ((OWLLiteral)v).getLiteral().toString();
+				}
+			}
+		}
+		return label;
+	}
+	
+	
+}
