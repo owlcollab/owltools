2803afb51cfc9b0a688cafaf8e9b88c603fc32c3
Chris Mungall <cjm@berkeleybop.org>
2014-03-04 20:42:53 +0000

    tests new frameMaker Json generator
    git-svn-id: http://owltools.googlecode.com/svn/trunk@2071 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Core/src/test/java/owltools/frame/FrameMakerTest.java
@@ -0,0 +1,36 @@
+package owltools.frame;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.semanticweb.owlapi.model.OWLClass;
+
+import owltools.OWLToolsTestBasics;
+import owltools.diff.ReasonerDiff;
+import owltools.frame.jsonld.ClassFrameLD;
+import owltools.frame.jsonld.FrameMakerLD;
+import owltools.graph.OWLGraphWrapper;
+import owltools.io.ParserWrapper;
+
+/**
+ * Tests for {@link FrameMakerLD}.
+ * 
+ */
+public class FrameMakerTest extends OWLToolsTestBasics {
+
+	private static Logger LOG = Logger.getLogger(OWLToolsTestBasics.class);
+
+	
+	@Test
+	public void testMakeClassFrame() throws Exception {
+		ParserWrapper pw = new ParserWrapper();
+		OWLGraphWrapper g = pw.parseToOWLGraph(getResourceIRIString("regulation_of_anti_apoptosis_xp-baseline.obo"));
+		FrameMakerLD fm = new FrameMakerLD(g);
+		for (OWLClass c : g.getAllOWLClasses()) {
+			ClassFrameLD f = fm.makeClassFrame(c);
+			String json = fm.translateToJson(f);
+			LOG.info(json);
+		}
+
+	}
+
+}
