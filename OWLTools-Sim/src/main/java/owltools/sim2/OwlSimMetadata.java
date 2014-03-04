89ba8b78ed75d0a05085814755608c60c908abee
Chris Mungall <cjm@berkeleybop.org>
2014-03-04 20:26:14 +0000

    primarily for json export
    git-svn-id: http://owltools.googlecode.com/svn/trunk@2067 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Sim/src/main/java/owltools/sim2/OwlSimMetadata.java
@@ -0,0 +1,17 @@
+package owltools.sim2;
+
+import owltools.mooncat.ontologymetadata.OntologySetMetadata;
+
+public class OwlSimMetadata {
+	OntologySetMetadata ontologySet;
+	public int classCount;
+	public int classIndexSize;
+	public int individualCount;
+	public String reasonerClass;
+	public int scaleFactor;
+	public int lcsCacheWidth;
+	public int allTypesDirectCount;
+	public int allTypesInferredCount;
+	
+	
+}
