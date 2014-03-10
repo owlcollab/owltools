7134ae0df0a6f782e2a833a2ac1af80567dec0c7
Chris Mungall <cjm@berkeleybop.org>
2014-03-10 20:44:05 +0000

    git-svn-id: http://owltools.googlecode.com/svn/trunk@2089 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Annotation/src/main/java/owltools/gaf/metadata/AnnotationDocumentMetadata.java
@@ -0,0 +1,12 @@
+package owltools.gaf.metadata;
+
+public class AnnotationDocumentMetadata {
+	
+	public String dbname; // e.g. goa_human, mgi
+	public String submissionDate; // MUST be ISO 8601, ie YYYY-MM-DD
+	public int annotatedEntityCount; // aka Gene Products Annotated
+	public int annotationCount;
+	public int annotationCountExcludingIEA;
+	public int gafDocumentSizeInBytes;
+
+}
