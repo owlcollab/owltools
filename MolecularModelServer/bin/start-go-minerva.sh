500dc9c5350c4f69c5e998ae3c1a73875877599e
Heiko Dietze <hdietze@lbl.gov>
2014-06-10 16:54:17 +0000

    create a script to start the GO specific Minerva instance
    git-svn-id: http://owltools.googlecode.com/svn/trunk@2246 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ MolecularModelServer/bin/start-go-minerva.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+# Any subsequent commands which fail will cause the shell script to exit immediately
+
+## Change for different max memory settings 
+MINERVA_MEMORY="4G"
+## Default Minerva Port
+MINERVA_PORT=6800
+
+## Check that exactly one command-line argument is set
+if [ $# -eq 0 ]
+  then
+    echo "Exactly one argument: GO_SVN root is required"
+    exit 1
+fi
+
+## Use command-line input as the location of the GO_SVN
+## Remove trailing slash
+GO_SVN=${1%/}
+
+## start Minerva
+# use catalog xml and PURLs
+java "-Xmx$MINERVA_MEMORY" -jar m3-server.jar \
+-c "$GO_SVN"/ontology/extensions/catalog-v001.xml \
+-g http://purl.obolibrary.org/obo/go.owl \
+-i http://purl.obolibrary.org/obo/go/extensions/x-disjoint.owl \
+-f "$GO_SVN"/experimental/lego/server/owl-models \
+--gaf-folder "$GO_SVN"/gene-associations \
+-p "$GO_SVN"/experimental/lego/server/protein/subset \
+--port $MINERVA_PORT
