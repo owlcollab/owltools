2f53eb1385462c73b30fe7244e8dba57af118f26
tudor.groza@gmail.com <tudor.groza@gmail.com>
2014-09-18 11:44:52 +0000

    Added a test case for the p-value. Still needs work
    git-svn-id: http://owltools.googlecode.com/svn/trunk@2351 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Sim/src/test/java/owltools/sim2/OwlSimVarianceTest.java
+++ OWLTools-Sim/src/test/java/owltools/sim2/OwlSimVarianceTest.java
@@ -13,6 +13,7 @@ import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import owltools.graph.OWLGraphWrapper;
 import owltools.io.ParserWrapper;
 import owltools.sim2.kb.OwlSimVariance;
+import owltools.sim2.kb.PValue;
 
 public class OwlSimVarianceTest extends AbstractOWLSimTest {
 
@@ -65,4 +66,41 @@ public class OwlSimVarianceTest extends AbstractOWLSimTest {
 
 	}
 
+	@Test
+	public void testPValue() throws Exception {
+		ParserWrapper pw = new ParserWrapper();
+		sourceOntol = pw.parseOBO(getResource("sim/mp-subset-1.obo").getAbsolutePath());
+		g =  new OWLGraphWrapper(sourceOntol);
+		parseAssociations(getResource("sim/mgi-gene2mp-subset-1.tbl"), g);
+
+		LOG.info("Initialize OwlSim ..");
+
+		OWLReasoner reasoner = new ElkReasonerFactory().createReasoner(sourceOntol);
+		reasoner.flush();
+		
+		try {
+			owlsim = owlSimFactory.createOwlSim(sourceOntol);
+			owlsim.createElementAttributeMapFromOntology();
+			owlsim.computeSystemStats();
+		} catch (UnknownOWLClassException e) {
+			e.printStackTrace();
+		} finally {
+			reasoner.dispose();
+		}
+
+		// Source sourceOntol and g are used as background knowledge ... 
+		OwlSimVariance variance = new OwlSimVariance(owlsim, sourceOntol, g);
+		
+		IRI iri = IRI.create("http://purl.obolibrary.org/obo/MGI_101761");
+		
+		String[] testClasses = new String[] {"MP:0002758", "MP:0002772", "MP:0005448", "MP:0003660"};
+		Set<OWLClass> testClassesSet = new HashSet<OWLClass>();
+		for (String testClass : testClasses) {
+			testClassesSet.add(this.getOBOClass(testClass));
+		}
+
+		PValue pValue = variance.getPValue(testClassesSet, iri);
+		LOG.info(pValue.getSimplePValue());
+
+	}
 }
