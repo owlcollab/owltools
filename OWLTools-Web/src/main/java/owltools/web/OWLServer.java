dd21c7ee7dd4fe970b7be5692eaadda4af4ae86c
Heiko Dietze <hdietze@lbl.gov>
2014-12-09 23:13:07 +0000

    add a basic status command
    git-svn-id: http://owltools.googlecode.com/svn/trunk@2415 18f1da76-1bb4-b526-5913-e828fe20442d

--- OWLTools-Web/src/main/java/owltools/web/OWLServer.java
+++ OWLTools-Web/src/main/java/owltools/web/OWLServer.java
@@ -5,17 +5,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.TimeZone;
 
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.util.URIUtil;
 import org.semanticweb.elk.owlapi.ElkReasonerFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -25,8 +28,10 @@ import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import de.tudresden.inf.lat.jcel.owlapi.main.JcelReasoner;
-import owltools.gaf.lego.MolecularModelManager;
 import owltools.graph.OWLGraphWrapper;
 import owltools.sim2.OwlSim;
 import uk.ac.manchester.cs.factplusplus.owlapiv3.FaCTPlusPlusReasonerFactory;
@@ -105,50 +110,110 @@ public class OWLServer extends AbstractHandler
 			m = mpa[0];
 			handler.setFormat(mpa[1]);
 		}
-		handler.setCommandName(m);
-		Class[] mArgs = new Class[0];
-		Method method = null;
-		try {
-			method = handler.getClass().getMethod(m + "Command", mArgs);
-		} catch (SecurityException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (NoSuchMethodException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		if ("status".equals(m)) {
+			// report status
+			reportStatus(baseRequest, request, response);
 		}
-		if (method != null) {
+		else {
+			handler.setCommandName(m);
+			Class[] mArgs = new Class[0];
+			Method method = null;
 			try {
-				LOG.info("Method="+method);
-				Object[] oArgs = new Object[0];
-				method.invoke(handler, oArgs);				
-				handler.printCachedObjects();	
-			} catch (SecurityException e) {
+				method = handler.getClass().getMethod(m + "Command", mArgs);
+			} catch (SecurityException e1) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
+				e1.printStackTrace();
+			} catch (NoSuchMethodException e1) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (OWLOntologyCreationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (OWLOntologyStorageException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e1.printStackTrace();
+			}
+			if (method != null) {
+				try {
+					LOG.info("Method="+method);
+					Object[] oArgs = new Object[0];
+					method.invoke(handler, oArgs);				
+					handler.printCachedObjects();	
+				} catch (SecurityException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (OWLOntologyCreationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (OWLOntologyStorageException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
-		
 		// TODO/BUG: Remove for a real system
 		// Close in case of JSONP. See above.
 		if( jsonp_callback != null ){ response.getWriter().write(')'); }			
 	}
 
+	private void reportStatus(Request baseRequest,
+			HttpServletRequest request,
+			HttpServletResponse response) {
+		Map<String, Object> jsonObj = new HashMap<String, Object>();
+		// name
+		jsonObj.put("name", "owlserver");
+
+		// okay
+		jsonObj.put("okay", Boolean.TRUE);
+
+		// date in GMT
+		Date localTime = new Date(); 
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		jsonObj.put("date", sdf.format(localTime));
+		
+		// location
+		// use the request URL, the server does not know its address
+		jsonObj.put("location", getLocationInfo(request));
+
+		// offerings (optional list of values)
+		// for now empty
+
+		try {
+			Gson gson = new GsonBuilder().create();
+			String js = gson.toJson(jsonObj);
+			response.getWriter().write(js);
+		} catch (IOException e) {
+			int code = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+			try {
+				response.sendError(code , "Could not send status message due to an internal issue: "+e.getMessage());
+			} catch (IOException e1) {
+				LOG.error("Could not send error message for original IOException: "+e.getMessage(), e1);
+			}
+		}
+	}
+	
+	private CharSequence getLocationInfo(HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		String scheme = request.getScheme();
+        int port = request.getServerPort();
+
+        sb.append(scheme);
+        sb.append("://");
+        sb.append(request.getServerName());
+        if (port>0 && 
+            ((scheme.equalsIgnoreCase(URIUtil.HTTP) && port != 80) || 
+             (scheme.equalsIgnoreCase(URIUtil.HTTPS) && port != 443)))
+        {
+        	sb.append(':');
+        	sb.append(port);
+        }
+		return sb;
+	}
+ 	
 	public synchronized OWLReasoner getReasoner(String reasonerName) {
 		// TODO - reasoner synchronization
 		if (reasonerMap.containsKey(reasonerName))
