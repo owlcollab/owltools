0f411f6f8068624b193e98ef2de8d57b17b48a0c
Chris Mungall <cjm@berkeleybop.org>
2015-01-21 19:06:35 +0000

    new test
    git-svn-id: http://owltools.googlecode.com/svn/trunk@2440 18f1da76-1bb4-b526-5913-e828fe20442d

---
+++ OWLTools-Runner/src/test/java/owltools/cli/CommandRunnerJsonLdTest.java
@@ -0,0 +1,26 @@
+package owltools.cli;
+
+import java.io.File;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link CommandRunner}.
+ * 
+ * these are somewhat ad-hoc at the moment - output is written to stdout;
+ * no attempt made to check results
+ * 
+ */
+public class CommandRunnerJsonLdTest extends AbstractCommandRunnerTest {
+	
+	@Test
+	public void testParse() throws Exception {
+		load("ceph.obo");
+		run("-o target/ceph.owl");
+		run("--rdf-to-json-ld -o target/ceph.json target/ceph.owl");
+		run("--json-ld-to-rdf -o target/ceph.rdf target/ceph.json");
+				
+	}
+	
+
+}
