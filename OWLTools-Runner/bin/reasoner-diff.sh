11417fcc269abf2c0301acb8073cd373471ca74c
Eric Douglass <edouglass@lbl.gov>
2017-03-03 16:02:19 -0800

    removing more stale scripts

--- OWLTools-Runner/bin/reasoner-diff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-B=$1
-C=$2
-owltools --use-catalog $B --reasoner elk --assert-inferred-subclass-axioms -o -f obo --no-check $B-reasoned.obo
-owltools --use-catalog $C --reasoner elk --assert-inferred-subclass-axioms -o -f obo --no-check $C-reasoned.obo
-obo-simple-diff.pl -l $C-reasoned.obo $B-reasoned.obo > DIFF
-
-
